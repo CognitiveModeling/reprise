38ef68ed4da6a224785c10a1efcbdc6c21dad461
Fedor Scholz <fedor.scholz@posteo.net>
2020-11-11 15:14:02 +0100

    add github link to rtd documentation

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -54,3 +54,11 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_context = {
+    "display_github": True,
+    "github_user": 'fedorsc',
+    "github_repo": 'reprise',
+    "github_version": 'master',
+    "conf_py_path": '/doc/source/'
+}
